e619d6ac7cbea25980b70cf22d2c653896935864
noahfranco <noahfra@gmail.com>
2019-08-12 16:49:43 -0400

    working on consuming data with product context

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,9 +1,17 @@
-import React from 'react';
+import React, { useContext } from 'react';
+// Importing Context 
+import ProductContext from "./contexts/ProductContext.js"; 
+// Importing Context 
 
 // Components
 import Product from './Product';
 
 const Products = props => {
+
+// Calling the useContext Hook
+const { product, addItem } = useContext(ProductContext); 
+// Calling the useContext Hook
+
 	return (
 		<div className="products-container">
 			{props.products.map(product => (
